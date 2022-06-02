39854b8575606001a94d063d23d8c6a1d90196c1
yjx <1332309765@qq.com>
2022-06-02 17:47:23 +0800

    first

--- src/main/java/com/yjx/Git/GitTest01.java
+++ src/main/java/com/yjx/Git/GitTest01.java
@@ -8,5 +8,6 @@ public class GitTest01 {
         System.out.println("hello git3!");
         System.out.println("master3 test!");
         System.out.println("hot-fix test!");
+        System.out.println("Gitee!");
     }
 }
